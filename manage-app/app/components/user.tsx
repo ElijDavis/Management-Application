1d074dcc097de4a4ff3138500f03d98e106b9a24
ElijDavis <elijahadavis@hotmail.com>
2025-11-17 21:32:23 -0500

    conditional component: conditional auth modal to render

--- manage-app/app/components/user.tsx
+++ manage-app/app/components/user.tsx
@@ -3,8 +3,13 @@
 
 import Image from "next/image";
 import { useState, useEffect } from 'react';
+import AuthModal from "./modals/authModal";
+
+const AuthModalAny: any = AuthModal;
 
 const User = () => {
+  const [user, setUser] = useState<boolean>(true);
+
   const handleClick = () => {
     
   }
@@ -12,8 +17,7 @@ const User = () => {
   return (
     <div onClick={handleClick} className="flex items-center justify-center w-15 h-15 rounded-full bg-white justify-self-end m-4 hover:scale-110 transition-transform duration-200 ease-in-out cursor-pointer">
       <Image src={"/images/default-user.png"} alt="Profile Picture" width={40} height={40} className="rounded-full" />
-      {
-      }
+      {!user ? <AuthModalAny modal={"Login"} /> : null}
     </div>
   );
 };
