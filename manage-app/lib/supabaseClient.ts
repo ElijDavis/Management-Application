f05cae109c483ed2942858234e9f1a392fb65f98
ElijDavis <elijahadavis@hotmail.com>
2025-11-18 00:07:50 -0500

    I guess I can't test on my laptop because this is ridiculous. Literally nothing is different between my desktop and my laptop and the localhost isn't working on the laptop vs the desktop.

--- manage-app/lib/supabaseClient.ts
+++ manage-app/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 // manage-app/lib/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js' //imported from supabase module
 const supabaseUrl = 'https://pffrwkcdaocgkfnkzcie.supabase.co' // Your Supabase URL (also in .env file)
-const supabaseKey = process.env.SUPABASE_KEY || process.env.NEXT_PUBLIC_SUPABASE_KEY //imported from .env file
+const supabaseKey = process.env.SUPABASE_KEY //imported from .env file
 if (!supabaseKey) { // Check if the key is missing
   throw new Error('Missing SUPABASE_KEY environment variable')
 }
