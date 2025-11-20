02e648e4235f3f3eb611908ddeed79517ae4ab2b
ElijDavis <elijahadavis@hotmail.com>
2025-11-20 09:18:29 -0500

    Ath flow: log out button created

--- manage-app/app/components/modals/settings.tsx
+++ manage-app/app/components/modals/settings.tsx
@@ -1,6 +1,9 @@
 'use client'
 
+import { logOut } from "@/utils/auth/auth"
+
 const SettingsModal = ({ onClose }: { onClose: () => void }) => {
+
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50" onClick={onClose}>
       <div className="w-[50%] relative p-6 bg-white rounded-lg shadow-lg" onClick={(e) => e.stopPropagation()}>
@@ -10,6 +13,9 @@ const SettingsModal = ({ onClose }: { onClose: () => void }) => {
         <h2 className="flex justfiy-self-center text-xl font-bold mb-4">Account Settings</h2>
         <p>Welcome, you're logged in!</p>
         {/* Add logout, change password, etc. here */}
+        <div className="flex justify-center space-2">
+          <button className="flex items-center justify-center bg-red-500" onClick={logOut}>Log Out</button>
+        </div>
       </div>
     </div>
   )
