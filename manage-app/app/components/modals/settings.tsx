f71d9210f74d494b177758999f88a1e2143270a8
ElijDavis <elijahadavis@hotmail.com>
2025-11-20 08:45:37 -0500

    Auth flow: Authentication flow is pretty much complete. Localhost testing will no longer be needed because supabase's redirect url's go to the vercel production site

--- manage-app/app/components/modals/settings.tsx
+++ manage-app/app/components/modals/settings.tsx
@@ -3,7 +3,7 @@
 const SettingsModal = ({ onClose }: { onClose: () => void }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50" onClick={onClose}>
-      <div className="relative p-6 bg-white rounded-lg shadow-lg" onClick={(e) => e.stopPropagation()}>
+      <div className="w-[50%] relative p-6 bg-white rounded-lg shadow-lg" onClick={(e) => e.stopPropagation()}>
         <button className="absolute top-2 right-2 text-gray-500 hover:text-gray-800" onClick={onClose}>
           ✕
         </button>
