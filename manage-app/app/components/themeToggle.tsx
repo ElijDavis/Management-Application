3d6ebeba389f21d4054f3cc13a19a3ba4237d5b7
ElijDavis <elijahadavis@hotmail.com>
2025-11-23 17:59:39 -0500

    Visual Edit: Added z scale to the toggle elements for overlapping

--- manage-app/app/components/themeToggle.tsx
+++ manage-app/app/components/themeToggle.tsx
@@ -27,11 +27,11 @@ const ThemeToggle = () => {
 
   return (
     <div className="flex -gap-2 p-4">
-      <button onClick={() => setTheme("light")} className="w-10 h-10 px-3 py-1 rounded-full outline-2 outline-offset-2 outline-foreground bg-white text-foreground"/>
-      <button onClick={() => setTheme("dark")} className="w-10 h-10 px-3 py-1 rounded-full outline-2 outline-offset-2 outline-foreground bg-black text-foreground"/>
-      <button onClick={() => setTheme("sepia")} className="w-10 h-10px-3 py-1 rounded-full outline-2 outline-offset-2 outline-forground bg-[#f4ecd8] text-foreground"/>
-      <button onClick={() => setTheme("blue")} className="w-10 h-10 px-3 py-1 rounded-full outline-2 outline-offset-2 outline-foreground bg-[#e0f2fe] text-foreground"/>
-      <button onClick={() => setTheme("system")} className="w-10 h-10 px-3 py-1 rounded-full outline-2 outline-offset-2 outline-foreground bg-background text-foreground"/>
+      <button onClick={() => setTheme("light")} className="w-10 h-10 z-2 px-3 py-1 rounded-full outline-2 outline-offset-2 outline-foreground bg-white text-foreground"/>
+      <button onClick={() => setTheme("dark")} className="w-10 h-10 z-4 px-3 py-1 rounded-full outline-2 outline-offset-2 outline-foreground bg-black text-foreground"/>
+      <button onClick={() => setTheme("sepia")} className="w-10 h-10 z-6 px-3 py-1 rounded-full outline-2 outline-offset-2 outline-forground bg-[#f4ecd8] text-foreground"/>
+      <button onClick={() => setTheme("blue")} className="w-10 h-10 z-8 px-3 py-1 rounded-full outline-2 outline-offset-2 outline-foreground bg-[#e0f2fe] text-foreground"/>
+      <button onClick={() => setTheme("system")} className="w-10 h-10 z-10 px-3 py-1 rounded-full outline-2 outline-offset-2 outline-foreground bg-background text-foreground"/>
     </div>
   );
 };
