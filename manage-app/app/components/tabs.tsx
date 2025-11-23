7da1f1dae8b469fb6f74b7fc6013ac5272fef399
ElijDavis <elijahadavis@hotmail.com>
2025-11-23 18:12:54 -0500

    Visual Edit: Changed the look of the user image and changed the spacing of the theme bubbles

--- manage-app/app/components/tabs.tsx
+++ manage-app/app/components/tabs.tsx
@@ -6,7 +6,7 @@ const Tabs = ({name}: NameProps) => {
   const isActive = pathname === `/${name.toLowerCase()}`;
 
   return(
-    <Link className={`flex justify-center h-full w-full hover:bg-gray-200 hover:text-black mx-auto hover:rounded-md cursor-pointer ${isActive ? "bg-linear-to-r from-white/0 from-0% via-background/35 via-50% to-white/0 to-100%  font-semibold" : ""}`} href={`/${name.toLowerCase()}`}>
+    <Link className={`flex justify-center h-full w-full hover:bg-foreground/20 hover:text-black mx-auto hover:rounded-md cursor-pointer ${isActive ? "bg-linear-to-r from-white/0 from-0% via-background/35 via-50% to-white/0 to-100%  font-semibold" : ""}`} href={`/${name.toLowerCase()}`}>
         <h1>{name}</h1>
     </Link>
   )
--- manage-app/app/components/user.tsx
+++ manage-app/app/components/user.tsx
@@ -22,12 +22,11 @@ const User = () => {
 
   return (
     <>
-      <div onClick={handleClick} className="flex items-center justify-center w-15 h-15 rounded-full bg-white justify-self-end m-4 hover:scale-110 transition-transform duration-200 ease-in-out cursor-pointer">
+      <div onClick={handleClick} className="flex items-center justify-center w-15 h-15 rounded-full bg-white justify-self-end m-4 hover:scale-110 hover:outline-offset-4 transition-transform duration-200 ease-in-out cursor-pointer outline-2 outline-offset-2 outline-foreground">
         <Image src="/images/default-user.png" alt="Profile Picture" width={40} height={40} className="rounded-full"/>
       </div>
       {showAuthModal && <AuthModal onClose={() => setShowAuthModal(false)} />}
       {showSettingsModal && <SettingsModal onClose={() => setShowSettingsModal(false)} />}
-
     </>
   )
 }
