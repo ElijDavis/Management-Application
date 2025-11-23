9610c1455cd492a46f78b49e1c6ab2cdbb7fe097
ElijDavis <elijahadavis@hotmail.com>
2025-11-22 23:05:40 -0500

    Visual Edit: Mapping out the spacing between page tabs

--- manage-app/app/components/navigationIsland.tsx
+++ manage-app/app/components/navigationIsland.tsx
@@ -8,7 +8,7 @@ const NavIsland = () => {
     const pathname = usePathname();
 
   return(
-    <div className={`sticky top-10 z-50 mx-auto w-[50%] rounded-lg bg-linear-to-r from-white/50 from-10% via-white/70 via-20% to-white/50 to-30% shadow-md flex justify-between *:pt-2 *:pb-2 `}>
+    <div className={`sticky top-10 z-50 mx-auto w-[50%] rounded-lg bg-linear-to-r from-white/50 from-2% via-white/70 via-100% to-white/50 to-18% shadow-md flex justify-between *:pt-2 *:pb-2 `}>
         <Tabs name={"Dashboard"}></Tabs>
         <div className="transform scale-y-150">|</div>
         <Tabs name={"Documents"}></Tabs>
