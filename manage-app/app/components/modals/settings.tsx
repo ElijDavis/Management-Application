50f57f92d149fa9f078e57e555b58447b1384698
ElijDavis <elijahadavis@hotmail.com>
2025-11-23 18:29:31 -0500

    Visual Edit: Uppon clicking user again, the theme changes back to black/dark

--- manage-app/app/components/modals/settings.tsx
+++ manage-app/app/components/modals/settings.tsx
@@ -11,7 +11,7 @@ const SettingsModal = ({ onClose }: { onClose: () => void }) => {
   }
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-background bg-opacity-50 z-50" onClick={onClose}>
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50" onClick={onClose}>
       <div className="flex flex-col justify-items-center w-[50%] relative p-6 bg-white rounded-lg shadow-lg" onClick={(e) => e.stopPropagation()}>
         <button className="absolute top-2 right-2 text-gray-500 hover:text-gray-800" onClick={onClose}>
           ✕
