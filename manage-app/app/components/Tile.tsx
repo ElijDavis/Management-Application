12532c7f81874b4508288253450882a47b35c9e0
ElijDavis <elijahadavis@hotmail.com>
2025-11-24 07:54:54 -0500

    Visual Edit: Tiles created and aspect ration works

--- manage-app/app/components/Tile.tsx
+++ manage-app/app/components/Tile.tsx
@@ -4,16 +4,16 @@
 
 const spanClasses: Record<TileProps["span"], string> = {
   1: "col-span-1",
-  2: "col-span-2",
+  2: "col-span-2",//over 5/2 (try 5/3 or 10/5)
   3: "col-span-3",
   4: "col-span-4",
 };
 
 const Tile = ({ span, shape ="square" }: TileProps) => {
-    const shapeClass = shape === "square" ? "aspect-square" : "aspect-[16/9]"; // or h-64 for fixed height
+    const shapeClass = shape === "square" ? "aspect-square" : ""; // or h-64 for fixed height
 
   return (
-    <div className={`bg-foreground/20 ${spanClasses[span]} ${shapeClass}`}>
+    <div className={`bg-foreground/20 ${spanClasses[span]} ${shapeClass} rounded-xl`}>
       Tile
     </div>
   );
--- manage-app/app/dashboard/page.tsx
+++ manage-app/app/dashboard/page.tsx
@@ -4,9 +4,13 @@ import Tile from "../components/Tile";
 
 const dashboard = () => {
   return(
-    <div className="mt-25 m-4 bg-black grid grid-cols-4 gap-4">
+    <div className="mt-25 m-4 grid grid-cols-4 gap-4">
       <Tile span={3} shape={'rect'}></Tile>
       <Tile span={1}></Tile>
+      <Tile span={1} shape={'square'}></Tile>
+      <Tile span={2} shape={'rect'}></Tile>
+      <Tile span={1} shape={'square'}></Tile>
+      <Tile span={1} shape={'square'}></Tile>
     </div>
   )
 }
