e2476f0c9c407bdd01baf1fd74ff5fe0b0e84853
ElijDavis <elijahadavis@hotmail.com>
2025-11-24 16:24:31 -0500

    Dashboard: Updated the chart to support real sample data. Using cubejs for the sample data. Stopped at tree shaking

--- manage-app/lib/graph/graphs.tsx
+++ manage-app/lib/graph/graphs.tsx
@@ -4,39 +4,77 @@
 //Bar
 import { useEffect, useRef } from "react";
 import Chart from "chart.js/auto";
+import { getAquisitionsByYear } from '../../lib/sampleData/sample'
 
 const BarChart = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
-  useEffect(() => {
-    if (!canvasRef.current) return;
+  useEffect(() => {//only runs on mount, not when other renders occur
+    let chart: any = null;
 
-    const data = [//sample data
-      { year: 2010, count: 10 },
-      { year: 2011, count: 20 },
-      { year: 2012, count: 15 },
-      { year: 2013, count: 25 },
-      { year: 2014, count: 22 },
-      { year: 2015, count: 30 },
-      { year: 2016, count: 28 },
-    ];
+    const loadData = async () => {//getting real world data
+      if (!canvasRef.current) return;
 
-    const chart = new Chart(canvasRef.current, {
-      type: "bar",
-      data: {
-        labels: data.map((row) => row.year),
-        datasets: [
-          {
-            label: "Acquisitions by year",
-            data: data.map((row) => row.count),
-          },
-        ],
-      },
-    });
+      /*const data = [//sample data
+        { year: 2010, count: 10 },
+        { year: 2011, count: 20 },
+        { year: 2012, count: 15 },
+        { year: 2013, count: 25 },
+        { year: 2014, count: 22 },
+        { year: 2015, count: 30 },
+        { year: 2016, count: 28 },
+      ];*/
+
+      const data = await getAquisitionsByYear();
+
+
+      //**** The below script is the original example, no options
+      /*const chart = new Chart(canvasRef.current, {
+        type: "bar",//specify the bar type
+        data: {//to be given to the graph
+          labels: data.map((row) => row.year), //labels to be displayed on x-axis
+          datasets: [//can have multiple datasets for more than one bar per x-label
+            {
+              label: "Acquisitions by year",//graph label
+              data: data.map((row) => row.count),//data to be mapped
+            },
+          ],
+        },
+      });*/
+
+      chart = new Chart(canvasRef.current, {
+        type: 'bar',//specify the bar type
+        options: {//new options for graph manipulation
+          animation: false,// the animation where it disapeared, set to false
+          plugins: {
+            legend: {//disables the legend at the top of the graph (in this instance "Aquisition by year")
+              display: false
+            },
+            tooltip: {//still don't know what it does
+              enabled: false
+            }
+          }
+        },
+        data: {//to be given to the graph
+          labels: data.map(row => row.year),//labels to be displayed on x-axis
+          datasets: [//can have multiple datasets for more than one bar per x-label
+            {
+              label: 'Acquisitions by year',//graph label
+              data: data.map(row => row.count)//data to be mapped
+            }
+          ]
+        }
+      }
+    );
+    };
+
+    loadData();
 
     // cleanup to avoid duplicate chart instances
     return () => {
-      chart.destroy();
+      if (chart) {
+        chart.destroy();
+      }
     };
   }, []);
 
---
+++ manage-app/lib/sampleData/sample.ts
@@ -0,0 +1,76 @@
+//Sample data for charts
+import cubejs, { Query } from '@cubejs-client/core';
+
+const apiUrl = 'https://heavy-lansford.gcp-us-central1.cubecloudapp.dev/cubejs-api/v1';
+const cubeToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjEwMDAwMDAwMDAsImV4cCI6NTAwMDAwMDAwMH0.OHZOpOBVKr-sCwn8sbZ5UFsqI3uCs6e4omT7P6WVMFw';
+
+const cubeApi = cubejs(cubeToken, { apiUrl });
+
+
+export async function getAquisitionsByYear() {
+  const acquisitionsByYearQuery: Query = {
+    dimensions: ['Artworks.yearAcquired'],
+    measures: ['Artworks.count'],
+    filters: [
+        {
+        member: 'Artworks.yearAcquired',
+        operator: 'set'
+        }
+    ],
+    order: {
+        'Artworks.yearAcquired': 'asc'
+    }
+    };
+
+  const resultSet = await cubeApi.load(acquisitionsByYearQuery);
+
+  return resultSet.tablePivot().map(row => ({
+    year: parseInt(String(row['Artworks.yearAcquired'])),
+    count: parseInt(String(row['Artworks.count']))
+  }));
+}
+
+export async function getDimensions() {
+  const dimensionsQuery: Query = {
+    dimensions: [
+      'Artworks.widthCm',
+      'Artworks.heightCm'
+    ],
+    measures: [
+      'Artworks.count'
+    ],
+    filters: [
+      {
+        member: 'Artworks.classification',
+        operator: 'equals',
+        values: [ 'Painting' ]
+      },
+      {
+        member: 'Artworks.widthCm',
+        operator: 'set'
+      },
+      {
+        member: 'Artworks.widthCm',
+        operator: 'lt',
+        values: [ '500' ]
+      },
+      {
+        member: 'Artworks.heightCm',
+        operator: 'set'
+      },
+      {
+        member: 'Artworks.heightCm',
+        operator: 'lt',
+        values: [ '500' ]
+      }
+    ]
+  };
+
+  const resultSet = await cubeApi.load(dimensionsQuery);
+
+  return resultSet.tablePivot().map(row => ({
+    width: parseInt(String(row['Artworks.widthCm'])),
+    height: parseInt(String(row['Artworks.heightCm'])),
+    count: parseInt(String(row['Artworks.count']))
+  }));
+}
