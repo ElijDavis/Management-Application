4107db57a28eb3e4d847c2d12092f297a3592d88
ElijDavis <elijahadavis@hotmail.com>
2025-11-29 21:26:33 -0500

    Visual Edit: changing theme toggle

--- manage-app/app/components/themeToggle.tsx
+++ manage-app/app/components/themeToggle.tsx
@@ -9,7 +9,7 @@ const ThemeToggle = () => {
   const {theme, setTheme} = useTheme();
 
   return (
-    <div className="inline-flex -space-x-4 p-4 rounded-[3rem] border-2 border-gray-400 bg-transparent">
+    <div className="inline-flex -space-x-4 p-4 *:rounded-lg border-2 border-gray-400 bg-transparent">
       <button onClick={() => setTheme("light")} title="Switch to Light Theme" className="w-10 h-10 bg-white text-foreground"/>
       <button onClick={() => setTheme("dark")} title="Switch to Dark Theme" className="w-10 h-10 bg-black text-foreground"/>
       <button onClick={() => setTheme("sepia")} title="Switch to Sepia Theme" className="w-10 h-10 bg-[#f4ecd8] text-foreground"/>
