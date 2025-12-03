5fa3baef0221f94d7e481793e32c928c1c9ab6f0
ElijDavis <elijahadavis@hotmail.com>
2025-12-02 22:22:52 -0500

    Visual Edit: The plus image for adding data

--- manage-app/app/dashboard/page.tsx
+++ manage-app/app/dashboard/page.tsx
@@ -6,7 +6,7 @@ import { getCharts, deleteChart } from "@/utils/graph/chartStorage";
 import Tile from "../components/Tile";
 import { chartMap } from "@/utils/graph/chartMap";
 import CreateChart from "../components/modals/newGraph";
-import Toast from "../components/toast";
+import Image from "next/image";
 
 const dashboard = () => {
   const [charts, setCharts] = useState<Record<string, any>>({});
@@ -40,14 +40,15 @@ const dashboard = () => {
   return(
     <div className="w-full">
       <div className="flex justify-end mr-36 mt-15 gap-2">{/* Make the New button dynamic because right now it is out of place */}
-        <button className="bg-foreground/50 aspect-square pl-2 pr-2 text-2xl hover:bg-foreground/10 active:bg-foreground rounded-xl" onClick={handleClick}>+</button>
+        <button className="bg-foreground/50 aspect-square pl-2 pr-2 text-2xl hover:bg-foreground/10 active:bg-foreground rounded-xl" onClick={handleClick}>
+          <Image src="/images/Plus.svg" alt="Add New Chart" width={24} height={24} />
+        </button>
       </div>
       <div className="m-4 grid grid-cols-4 gap-4 grid-flow-dense">
         {Object.entries(charts).map(([name, {chartType, url}]) => {
           const ChartComponent = chartMap[chartType];
           return (
           <Tile key={name} chartType={chartType} href={`/dashboard/${name}`} name={name.toUpperCase()} onDelete={() => handleDelete(name)} onEdit={() => handleEdit()}>
-            {/*{chartMap[chartType]({ source: url, xKey: "year", yKey: "count", datasetLabel: name })}*/}
             <ChartComponent source={url} xKey="year" yKey="count" datasetLabel={name} />
           </Tile>
           )
